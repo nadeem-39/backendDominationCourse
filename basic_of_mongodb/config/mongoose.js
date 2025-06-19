843f555874d20b76304e3d39f8a4941d989b75b6
NADEEM <144749860+nadeem-39@users.noreply.github.com>
2025-06-19 22:43:26 +0530

    Update mongoose.js remove mongo uri

--- basic_of_mongodb/config/mongoose.js
+++ basic_of_mongodb/config/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose= require('mongoose');
 const debuglog = require('debug')('development:mongooseconfig');
 
-mongoose.connect('mongodb+srv://nadeemsiddiqui0390:yR5hKn4MOb3WsWTW@cluster0.6gxzu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+mongoose.connect('mongodb');
 
 const db = mongoose.connection;
 
